dd1ba022156d8d16d16ed0b24b975c158d244828
alicebyers5 <alice.byers@gov.scot>
2023-10-16 17:24:40 +0100

    Edit example 1 chart and add table

--- 2023-09-28_accessible-data-vis/examples.R
+++ 2023-09-28_accessible-data-vis/examples.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(gapminder)
 library(sgplot)
 library(here)
+library(tidyr)
+library(gt)
+
 
 # Create data set
 
@@ -15,17 +18,20 @@ life_exp <-
   filter(country %in% c("United Kingdom", "China", "India",
                         "Sweden", "Namibia", "Brazil"))
 
+
 # Example 1
 
 ex1 <-
   ggplot(life_exp) +
-  geom_line(aes(x = year, y = lifeExp, colour = country))
+  geom_line(aes(x = year, y = lifeExp, colour = country)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 ggsave(
   ex1, 
   filename = here("2023-09-28_accessible-data-vis", "images", "ex1.svg"),
   width = 200, height = 100, units = "mm"
 )
+
 
 # Example 2
 
@@ -43,6 +49,7 @@ ggsave(
   filename = here("2023-09-28_accessible-data-vis", "images", "ex2.svg"),
   width = 200, height = 100, units = "mm"
 )
+
 
 # Example 3
 
@@ -72,4 +79,29 @@ ggsave(
   ex3, 
   filename = here("2023-09-28_accessible-data-vis", "images", "ex3.svg"),
   width = 220, height = 100, units = "mm"
+)
+
+
+# Alt text table
+
+table <-
+  life_exp |>
+  mutate(lifeExp = round(lifeExp, 1)) |>
+  pivot_wider(names_from = country, values_from = lifeExp) |>
+  rename(Year = year) |>
+  slice_head(n = 5) |>
+  gt() |>
+  tab_spanner(
+    label = "Life Expectancy",
+    columns = !Year
+  ) |>
+  tab_options(
+    table.width = pct(100),
+    table.font.size = px(20)) |>
+  opt_vertical_padding(scale = 2.5) |>
+  opt_horizontal_padding(scale = 2.5)
+
+gtsave(
+  table,
+  here("2023-09-28_accessible-data-vis", "images", "table.html")
 )
