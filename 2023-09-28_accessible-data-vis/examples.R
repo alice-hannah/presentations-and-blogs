3494b4b5758b9735c88df8666013d1c5be1e0827
alicebyers5 <alice.byers@gov.scot>
2023-10-16 15:23:13 +0100

    Create and save example charts

---
+++ 2023-09-28_accessible-data-vis/examples.R
@@ -0,0 +1,75 @@
+
+# Load packages
+
+library(ggplot2)
+library(dplyr)
+library(gapminder)
+library(sgplot)
+library(here)
+
+# Create data set
+
+life_exp <-
+  gapminder |>
+  select(year, country, lifeExp) |>
+  filter(country %in% c("United Kingdom", "China", "India",
+                        "Sweden", "Namibia", "Brazil"))
+
+# Example 1
+
+ex1 <-
+  ggplot(life_exp) +
+  geom_line(aes(x = year, y = lifeExp, colour = country))
+
+ggsave(
+  ex1, 
+  filename = here("2023-09-28_accessible-data-vis", "images", "ex1.svg"),
+  width = 200, height = 100, units = "mm"
+)
+
+# Example 2
+
+life_exp <- life_exp |>
+  filter(country %in% c("United Kingdom", "China"))
+
+use_sgplot()
+
+ex2 <- 
+  ggplot(life_exp) +
+  geom_line(aes(x = year, y = lifeExp, colour = country))
+
+ggsave(
+  ex2, 
+  filename = here("2023-09-28_accessible-data-vis", "images", "ex2.svg"),
+  width = 200, height = 100, units = "mm"
+)
+
+# Example 3
+
+ex3 <-
+  ggplot(life_exp) +
+  geom_line(aes(x = year, y = lifeExp, colour = country)) +
+  theme_sg(legend = "none") +
+  scale_y_continuous(limits = c(0, 82),
+                     breaks = seq(0, 80, 20),
+                     expand = c(0, 0)) +
+  scale_x_continuous(limits = c(1952, 2017),
+                     breaks = seq(1952, 2007, 5)) +
+  annotate(geom = "label", x = 2008, y = 73, label = "China",
+           colour = sg_colour_values[1],
+           label.size = NA,
+           hjust = 0, vjust = 0.5) +
+  annotate(geom = "label", x = 2008, y = 79.4, label = "United Kingdom",
+           colour = sg_colour_values[2],
+           label.size = NA,
+           hjust = 0, vjust = 0.5) +
+  labs(
+    x = "Year",
+    y = "Life\nExpectancy"
+  )
+
+ggsave(
+  ex3, 
+  filename = here("2023-09-28_accessible-data-vis", "images", "ex3.svg"),
+  width = 220, height = 100, units = "mm"
+)
